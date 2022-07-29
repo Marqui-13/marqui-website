72ef9431bf1ceeaac58b07fb2cd7f9f2cc4bb490
Marqui-13 <98498681+Marqui-13@users.noreply.github.com>
2022-07-29 14:12:34 -0700

    Update Projects.jsx

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ProjectItem from './ProjectItem'
 import defiImg from '../public/assets/projects/defi.png'
 import coinsearchImg from '../public/assets/projects/coinsearch.png'
+import threelpImg from './../public/assets/projects/three_lp_img.png';
+
 
 
 
@@ -14,6 +16,21 @@ const Projects = () => {
             </p>
             <h2 className='py-4 text-center'>What I've Built</h2>
             <div className='grid md:grid-cols-2 gap-4'>
+                {/* <ProjectItem 
+                title='Homez' 
+                backgroundImg={propertyImg} 
+                projectUrl='/property' 
+                /> */}
+                {/* <ProjectItem 
+                title='Webflix' 
+                backgroundImg={netflixImg} 
+                projectUrl='/netflix' 
+                /> */}
+                <ProjectItem 
+                title='3JS' 
+                backgroundImg={threelpImg} 
+                projectUrl='/threelp' 
+                />
                 <ProjectItem 
                 title='DeFi' 
                 backgroundImg={defiImg} 
