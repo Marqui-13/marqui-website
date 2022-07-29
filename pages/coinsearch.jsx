378d5c0b0ff9432de19f1973bdadde06c2ee248c
Marqui-13 <98498681+Marqui-13@users.noreply.github.com>
2022-07-29 11:52:11 -0700

    Update coinsearch.jsx

--- pages/coinsearch.jsx
+++ pages/coinsearch.jsx
@@ -17,8 +17,8 @@ const coinsearch = () => {
           alt='/'
         />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
-          <h2 className='py-2'>CoinSearch App</h2>
-          <h3>React JS</h3>
+          <h2 className='py-2'>CoinSearch</h2>
+          <h3>React JS / Tailwind CSS / Fleek</h3>
         </div>
       </div>
 
