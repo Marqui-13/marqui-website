06d9a3da0898e7eea1b70df627b85b8308b0e0b8
Marqui-13 <98498681+Marqui-13@users.noreply.github.com>
2022-07-29 13:45:52 -0700

    Update defi.jsx

--- pages/defi.jsx
+++ pages/defi.jsx
@@ -51,7 +51,7 @@ const crypto = () => {
         </div>
         <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
           <div className='p-2'>
-            <p className='text-center font-bold pb-2'>Technologies</p>
+            <p className='text-center font-bold pb-2'>Tech</p>
             <div className='grid grid-cols-3 md:grid-cols-1 '>
               <p className='text-gray-600 py-2 flex items-center'>
                 <RiRadioButtonFill className='pr-1' /> React
