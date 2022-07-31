0f89ad794f54d877a0e950ffe6cff7a5568d46a0
Marqui-13 <98498681+Marqui-13@users.noreply.github.com>
2022-07-31 00:51:48 -0700

    Update Main.jsx

--- components/Main.jsx
+++ components/Main.jsx
@@ -22,9 +22,6 @@ const Main = () => {
                     <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
                         <AiOutlineMail />
                     </div>
-                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <BsFillPersonLinesFill />
-                    </div>
                 </div>
             </div>
         </div>
