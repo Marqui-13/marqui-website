0dc96aed465ae3e8a71ebb8bca61caae2de22981
Marqui-13 <98498681+Marqui-13@users.noreply.github.com>
2022-08-01 17:57:19 -0700

    Update Main.jsx

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import {FaGithub, FaLinkedinIn} from 'react-icons/fa'
+import {FaGithub, FaLinkedinIn, FaCodepen} from 'react-icons/fa'
 import {AiOutlineMail} from 'react-icons/ai'
-import {BsFillPersonLinesFill} from 'react-icons/bs'
 
 
 const Main = () => {
@@ -13,15 +12,35 @@ const Main = () => {
                 <h1>Hi, I'm <span className='text-[#15a8fd]'>Marqui</span></h1>
                 <p className='py-4 text-gray-400 max-w-[70%] m-auto'>I'm a developer and a student focusing on Web3, Blockchain, & Responsive Front-End/Back-End Development.</p>
                 <div className='flex items-center justify-between max-w-[330] m-auto py-4'>
+                    <a 
+                        href='https://www.linkedin.com/in/marqui-orr-793115219/'
+                        target='_blank'
+                        rel='noreferrer'
+                    >
                     <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
                         <FaLinkedinIn />
                     </div>
+                    </a>
+
+                    <a 
+                        href='https://github.com/Marqui-13'
+                        target='_blank'
+                        rel='noreferrer'
+                    >
                     <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
                         <FaGithub />
                     </div>
+                    </a>
+
+                    <a 
+                        href='https://codepen.io/marqui-13'
+                        target='_blank'
+                        rel='noreferrer'
+                    >
                     <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                        <AiOutlineMail />
+                        <FaCodepen />
                     </div>
+                    </a>
                 </div>
             </div>
         </div>
