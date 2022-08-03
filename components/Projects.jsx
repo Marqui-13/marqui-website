d80cfda54c8d0e2bc225479e278b821bead90f4a
Marqui-13 <98498681+Marqui-13@users.noreply.github.com>
2022-08-03 11:43:38 -0700

    Update Projects.jsx

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ProjectItem from './ProjectItem'
-import defiImg from '../public/assets/projects/defi.png'
+//import defiImg from '../public/assets/projects/defi.png'
 import coinsearchImg from '../public/assets/projects/coinsearch.png'
 import threelpImg from './../public/assets/projects/three_lp_img.png';
 import threelpImg2 from './../public/assets/projects/three_lp_img_2.png';
 import fichainImg from './../public/assets/projects/fichain.png';
+import filler from './../public/assets/projects/filler.png';
 
 const Projects = () => {
   return (
-    <div id='projects' className='ml-0 w-full mt-20'>
+    <div id='projects' className='ml-0 w-full mt-30'>
         <div className='max-w-[1240px] mx-auto px-2 py-16'>
             <p className='text-xl text-center tracking-widest uppercase text-[#15a8fd]'>
                 Projects
@@ -16,30 +17,35 @@ const Projects = () => {
             <h2 className='py-4 text-center'>What I've Built</h2>
             <div className='grid md:grid-cols-2 gap-4'>
                 <ProjectItem 
+                title='BlockCord' 
+                backgroundImg={filler} 
+                projectUrl='/blockcord' 
+                />
+                <ProjectItem 
                 title='FiChain' 
                 backgroundImg={fichainImg} 
                 projectUrl='/fichain' 
-                />
-                <ProjectItem 
-                title='3JS2' 
-                backgroundImg={threelpImg2} 
-                projectUrl='/threelp2' 
-                />
-                <ProjectItem 
-                title='3JS' 
-                backgroundImg={threelpImg} 
-                projectUrl='/threelp' 
-                />
-                <ProjectItem 
-                title='DeFi' 
-                backgroundImg={defiImg} 
-                projectUrl='/defi' 
                 />
                 <ProjectItem 
                 title='CoinSearch' 
                 backgroundImg={coinsearchImg} 
                 projectUrl='/coinsearch' 
                 />
+                <ProjectItem 
+                title='Three JS LP' 
+                backgroundImg={threelpImg2} 
+                projectUrl='/threelp2' 
+                />
+                <ProjectItem 
+                title='Three JS LP' 
+                backgroundImg={threelpImg} 
+                projectUrl='/threelp' 
+                />
+                {/* <ProjectItem 
+                title='DeFi' 
+                backgroundImg={defiImg} 
+                projectUrl='/defi' 
+                /> */}
             </div>
         </div>
     </div>
