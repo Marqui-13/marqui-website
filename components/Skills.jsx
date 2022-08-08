fc87c3518aa15a5bc3979311918a9f408ab4a274
Marqui-13 <98498681+Marqui-13@users.noreply.github.com>
2022-08-08 15:21:17 -0700

    Update Skills.jsx

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -23,6 +23,8 @@ import fleekImg from '../public/assets/skills/fleek.png.png'
 import netlifyImg from '../public/assets/skills/netlify.png'
 import githubImg from '../public/assets/skills/github.png'
 import tensorflowImg from '../public/assets/skills/tensorflow.PNG'
+import remixImg from '../public/assets/skills/tensorflow.PNG'
+
 
 
 
@@ -330,6 +332,19 @@ const Skills = () => {
               </div>
               <div className="flex flex-col items-center justify-center">
                 <h3>TensorFlow</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <SkillItem
+                    img={remixImg} 
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Remix</h3>
               </div>
             </div>
           </div>
