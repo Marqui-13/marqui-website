4ec62656500384ca1991c15ecf03d0c8cdd81d34
Marqui-13 <98498681+Marqui-13@users.noreply.github.com>
2022-09-02 15:59:22 -0700

    Update Skills.jsx

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -236,6 +236,19 @@ const Skills = () => {
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <SkillItem
+                    img={remixImg} 
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Remix</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <SkillItem
                     img={ozImg} 
                 />
               </div>
@@ -332,19 +345,6 @@ const Skills = () => {
               </div>
               <div className="flex flex-col items-center justify-center">
                 <h3>TensorFlow</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <SkillItem
-                    img={remixImg} 
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Remix</h3>
               </div>
             </div>
           </div>
