cb33e4165251bb91ef6f03625486ec00cc328c89
Marqui-13 <98498681+Marqui-13@users.noreply.github.com>
2022-09-07 22:58:05 -0700

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -20,11 +20,7 @@ const About = () => {
           {/* <p className='py-2 text-gray-600 underline cursor-pointer'>Check out some of my latest projects</p> */}
         </div>
         <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
-//           <Image
-//             className="rounded-xl"
-//             src={aboutLogo}
-//             alt="/"
-//           />
+          
         </div>
       </div>
     </div>
